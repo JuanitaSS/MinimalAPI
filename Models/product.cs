5cb3d78ad7c165307d5f93b059559096138dc56b
Juanita Solórzano Salazar <150202024+JuanitaSS@users.noreply.github.com>
2023-11-28 10:44:41 -0500

    Update product.cs

--- Models/product.cs
+++ Models/product.cs
@@ -1,20 +1,14 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace MinimalAPI.Modelos
 {
     public class Producto
     {
         public Guid Id { get; set; }
-
-        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
         public string Nombre { get; set; }
-
-        [Required(ErrorMessage = "La cantidad del producto es obligatoria.")]
-        [Range(0, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor o igual a cero.")]
         public int Cantidad { get; set; }
 
-        // Puedes agregar más propiedades según tus necesidades
+        // Otras propiedades
 
         public Producto()
         {
